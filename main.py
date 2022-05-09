be7cbd5a5404adc989fadafbcb65739921d0501c
Yoav-Keissar <yoav.keissar@ibm.com>
2022-05-09 21:54:11 +0300

    add logging and fix bugs

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from siamese_nn import SiameseNN
 import cv2
 import numpy as np
 from sklearn.utils import shuffle
+from datetime import datetime
+import logging as log
+import sys
+import json
+
+logfile_name = f"run_{str(datetime.now()).replace(' ', '_')}"
+log.basicConfig(filename=f"logs/{logfile_name}.log", format='%(asctime)s.%(msecs)03d %(levelname)s {%(module)s} [%(funcName)s] %(message)s', datefmt='%Y-%m-%d,%H:%M:%S', level=log.INFO)
+log.getLogger().addHandler(log.StreamHandler(sys.stdout))
 
 
 def extract_images(file, all_img_dir):
@@ -42,18 +50,35 @@ def load_data():
     X_train, y_train = extract_images(train_file, all_img_dir)
     X_test, y_test = extract_images(test_file, all_img_dir)
 
-    shuffle(X_train, y_train)
-    shuffle(X_test, y_test)
+    X_train, y_train = shuffle(X_train, y_train)
+    X_test, y_test = shuffle(X_test, y_test)
 
     X_train = [X_train[:, 0, :, :], X_train[:, 1, :, :]]
     X_test = [X_test[:, 0, :, :], X_test[:, 1, :, :]]
     return X_train, y_train, X_test, y_test
 
 
+def run_model(X_train, y_train, X_test, y_test, params=None):
+    """
+    :param X_train: Train samples. size - (2, m, 250, 250)
+    :param y_train: Train labels. size - (m, 1)
+    :param X_test: Test samples. size - (2, m, 250, 250)
+    :param y_test: Test labels. size - (m, 1)
+    :param params: Dictionary of params for the model -
+                   example: {'learning_rate': 1e-2, 'l2_regularization': 1e-3, 'batch_size': 128}
+    """
+    if params is None:
+        params = {}
+    run_name = f"{json.dumps(params)}_{str(datetime.now()).replace(' ', '-')}"
+    model = SiameseNN(**params, name=run_name)
+    model.fit(X_train, y_train, epochs=1)
+    log.info(f"Run name - {run_name}\ntrain accuracy - {model.score(X_train, y_train)} test accuracy - {model.score(X_test, y_test)}\n")
+
+
 def main():
     X_train, y_train, X_test, y_test = load_data()
-    model = SiameseNN(learning_rate=0.01, batch_size=128)
-    model.fit(X_train, y_train)
+
+    run_model(X_train, y_train, X_test, y_test)
 
 
 if __name__ == '__main__':
--- siamese_nn.py
+++ siamese_nn.py
@@ -5,10 +5,12 @@ import math
 from tensorflow.keras.callbacks import TensorBoard
 from datetime import datetime
 from keras.regularizers import l2
+import logging as log
 
 
 class SiameseNN:
-    def __init__(self, learning_rate, batch_size, l2_regularization=0):
+    def __init__(self, learning_rate=1e-2, batch_size=128, l2_regularization=1e-2, name=''):
+        log.info(f"Run {name} started. Params: learning_rate-{learning_rate}, batch_size-{batch_size}, l2_regularization-{l2_regularization}")
         self.batch_size = batch_size
         self.learning_rate = learning_rate
         self.l2_regularization = l2_regularization
@@ -19,36 +21,43 @@ class SiameseNN:
             decay_steps=100,
             decay_rate=0.99)
 
-        conv_net = tf.keras.models.Sequential([
+        self.conv_net = tf.keras.models.Sequential([
             tf.keras.layers.Conv2D(filters=64, kernel_size=(10, 10), activation='relu', input_shape=self.input_shape,
-                                   kernel_regularizer=l2(1e-2), kernel_initializer=initializer),
+                                   kernel_regularizer=l2(1e-2), kernel_initializer=initializer,
+                                   name='Conv1'),
             tf.keras.layers.MaxPool2D(),
             tf.keras.layers.Conv2D(filters=64, kernel_size=(8, 8), activation='relu',
-                                   kernel_regularizer=l2(1e-2), kernel_initializer=initializer),
+                                   kernel_regularizer=l2(1e-2), kernel_initializer=initializer,
+                                   name='Conv2'),
             tf.keras.layers.MaxPool2D(),
             tf.keras.layers.Conv2D(filters=128, kernel_size=(7, 7), activation='relu',
-                                    kernel_regularizer=l2(1e-2), kernel_initializer=initializer),
+                                    kernel_regularizer=l2(1e-2), kernel_initializer=initializer,
+                                   name='Conv3'),
             tf.keras.layers.MaxPool2D(),
             tf.keras.layers.Conv2D(filters=128, kernel_size=(4, 4), activation='relu',
-                                   kernel_regularizer=l2(1e-2), kernel_initializer=initializer),
+                                   kernel_regularizer=l2(1e-2), kernel_initializer=initializer,
+                                   name='Conv4'),
             tf.keras.layers.MaxPool2D(),
             tf.keras.layers.Conv2D(filters=256, kernel_size=(4, 4), activation='relu',
-                                   kernel_regularizer=l2(1e-2), kernel_initializer=initializer),
+                                   kernel_regularizer=l2(1e-2), kernel_initializer=initializer,
+                                   name='Conv5'),
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(units=4096, activation='sigmoid',
-                                  kernel_regularizer=l2(1e-4), kernel_initializer=initializer),
+                                  kernel_regularizer=l2(1e-4), kernel_initializer=initializer,
+                                  name='Dense1'),
         ])
 
         img1_inp = tf.keras.layers.Input(self.input_shape)
         img2_inp = tf.keras.layers.Input(self.input_shape)
 
-        embedded_img1 = conv_net(img1_inp)
-        embedded_img2 = conv_net(img2_inp)
+        embedded_img1 = self.conv_net(img1_inp)
+        embedded_img2 = self.conv_net(img2_inp)
 
         l1_layer = tf.keras.layers.Lambda(lambda eis: K.abs(eis[0] - eis[1]))
         l1_dist = l1_layer([embedded_img1, embedded_img2])
 
-        prediction = tf.keras.layers.Dense(1, activation='sigmoid')(l1_dist)
+        prediction = tf.keras.layers.Dense(1, activation='sigmoid',
+                                           name='Prediction')(l1_dist)
 
         self.model = tf.keras.models.Model(inputs=[img1_inp, img2_inp], outputs=[prediction])
 
@@ -56,17 +65,21 @@ class SiameseNN:
                            metrics=['accuracy'])
 
     def fit(self, X, y, epochs=5):
+        log.info(f"Fit start - epochs:{epochs}")
         training_history = self.model.fit(X, y, batch_size=self.batch_size, epochs=epochs, validation_split=0.2,
                                           verbose=1,
-                                          callbacks=[TensorBoard(log_dir=f"logs/{str(datetime.now()).replace(' ', '_')}",
+                                          callbacks=[TensorBoard(log_dir=f"logs/tb/{str(datetime.now()).replace(' ', '_')}",
                                                                  update_freq='batch')])
-        print(1)
+        return training_history
 
     def predict(self, X):
-        pass
+        y_pred = self.model.predict(X)
+        y_pred = np.round(y_pred)
+        return y_pred
 
-    def accuracy(self, y_true, y_pred):
-        pass
+    def score(self, X, y):
+        y_pred = self.predict(X)
+        return (y == y_pred).mean()
 
     def iter_batches(self, X, y, batch_size):
         """
